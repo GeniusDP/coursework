b52900775a4660b8e5c3777840e87aeaad6ed939
GeniusDP <zaranikbz@gmail.com>
2022-11-07 13:23:05 +0200

    db init script

---
+++ authservice/src/main/resources/init_db.sql
@@ -0,0 +1,26 @@
+drop table if exists app_users, roles cascade ;
+
+create table if not exists roles
+(
+    id   bigserial primary key,
+    name varchar(10) unique not null
+);
+
+insert into roles (name)
+values ('USER'),
+       ('TEACHER'),
+       ('ADMIN');
+
+
+create table if not exists app_users
+(
+    id            bigserial primary key,
+    username      varchar(50) unique not null,
+    email         varchar(50)        not null,
+    password      varchar(50)        not null,
+    first_name    varchar(50)        not null,
+    last_name     varchar(50)        not null,
+    is_activated  boolean            not null default false,
+    refresh_token text,
+    role_id       bigint             not null references roles (id)
+);
