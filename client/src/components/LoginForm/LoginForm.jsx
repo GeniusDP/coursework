4529ec4b96391410d3f17379f06a11bbba444bc2
GeniusDP <zaranikbz@gmail.com>
2022-11-10 18:47:57 +0200

    login form styles

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,7 +1,9 @@
 import React, { useContext, useState } from "react";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { AuthContext } from "../../App";
 import loginFetch from "../../customFetch";
+import "./../component-styles.css";
+import "./login-form-styles.css";
 
 const LoginForm = () => {
   const [username, setUsername] = useState("usr");
@@ -30,18 +32,30 @@ const LoginForm = () => {
   }
 
   return (
-    <div>
-      {isLoginError && <div>Not valid data!</div>}
-      <input
-        value={username}
-        onChange={(event) => setUsername(event.target.value)}
-      />
-      <input
-        type={"password"}
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-      />
-      <button onClick={performLogin}>Login</button>
+    <div className="login-form-wrapper">
+      <div className="login-form">
+        <div id={"login-message"}>Login to your account</div>
+        {isLoginError && <div>Not valid data!</div>}
+        <div>
+          <input
+            className={"form-control custom-input"}
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <input
+            className={"form-control custom-input"}
+            type={"password"}
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div>
+          <button className={"btn btn-outline-success btn-lg login-button"} onClick={performLogin}>
+            Login
+          </button>
+        </div>
+        <div><Link to="/register">Don`t have an account? Register now!</Link></div>
+      </div>
     </div>
   );
 };
