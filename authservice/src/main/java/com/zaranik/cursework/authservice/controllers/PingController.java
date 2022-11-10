74e16ccfaaa97a6b5ea8a6a07a1a3f7a795d7274
GeniusDP <zaranikbz@gmail.com>
2022-11-10 12:21:32 +0200

    /api/auth routing changed

--- authservice/src/main/java/com/zaranik/cursework/authservice/controllers/PingController.java
+++ authservice/src/main/java/com/zaranik/cursework/authservice/controllers/PingController.java
@@ -1,9 +1,11 @@
 package com.zaranik.cursework.authservice.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/auth")
 public class PingController {
 
     @GetMapping(value = "/ping", produces = "application/json")
