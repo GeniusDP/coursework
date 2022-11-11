03865f1d54fceedfe5d98f8426eab3c67c9823c5
GeniusDP <zaranikbz@gmail.com>
2022-11-11 11:40:37 +0200

    spinner + modal styles

---
+++ client/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,9 @@
+import './../component-styles.css';
+
+export const Spinner = () => {
+  return (
+    <div className={"spinner-wrapper"}>
+      <div className={"spinner"}></div>
+    </div>
+  );
+};
