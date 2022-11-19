05ab725a661615f19fcd44c6c64c7aa280c6defd
GeniusDP <zaranikbz@gmail.com>
2022-11-19 15:42:18 +0200

    refactored user entity

--- user-management-service/src/main/java/com/example/demo/entities/User.java
+++ user-management-service/src/main/java/com/example/demo/entities/User.java
@@ -2,9 +2,11 @@ package com.example.demo.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
-
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
@@ -12,85 +14,84 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class User extends BaseEntity {
 
+  private String username;
+
+  private String email;
+
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  private String password;
+
+  private String firstName;
+
+  private String lastName;
+
+  private Boolean isActivated = false;
+
+  @JsonIgnore
+  private String refreshToken;
+
+  @ManyToOne(optional = false)
+  private Role role;
+
+
+  public User(String username, String email, String password,
+    String firstName, String lastName, boolean isActivated) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.isActivated = isActivated;
+  }
+
+
+  public static UserBuilder builder() {
+    return new UserBuilder();
+  }
+
+  public static class UserBuilder {
+
     private String username;
-
     private String email;
-
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
-
     private String firstName;
-
     private String lastName;
-
-    @Builder.Default
     private boolean isActivated = false;
 
-    @JsonIgnore
-    private String refreshToken;
-
-    @ManyToOne(optional = false)
-    private Role role;
-
-
-    public User(String username, String email, String password,
-                String firstName, String lastName, boolean isActivated) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.isActivated = isActivated;
+    public UserBuilder setUsername(String username) {
+      this.username = username;
+      return this;
     }
 
-
-    public static UserBuilder builder(){
-        return new UserBuilder();
+    public UserBuilder setEmail(String email) {
+      this.email = email;
+      return this;
     }
 
-    public static class UserBuilder {
-        private String username;
-        private String email;
-        private String password;
-        private String firstName;
-        private String lastName;
-        private boolean isActivated;
-
-        public UserBuilder setUsername(String username) {
-            this.username = username;
-            return this;
-        }
-
-        public UserBuilder setEmail(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public UserBuilder setPassword(String password) {
-            this.password = password;
-            return this;
-        }
-
-        public UserBuilder setFirstName(String firstName) {
-            this.firstName = firstName;
-            return this;
-        }
-
-        public UserBuilder setLastName(String lastName) {
-            this.lastName = lastName;
-            return this;
-        }
-
-        public UserBuilder setActivated(boolean activated) {
-            isActivated = activated;
-            return this;
-        }
-
-        public User build(){
-            return new User(username, email, password,
-                    firstName, lastName, isActivated);
-        }
-
+    public UserBuilder setPassword(String password) {
+      this.password = password;
+      return this;
     }
+
+    public UserBuilder setFirstName(String firstName) {
+      this.firstName = firstName;
+      return this;
+    }
+
+    public UserBuilder setLastName(String lastName) {
+      this.lastName = lastName;
+      return this;
+    }
+
+    public UserBuilder setActivated(boolean isActivated) {
+      this.isActivated = isActivated;
+      return this;
+    }
+
+    public User build() {
+      return new User(username, email, password, firstName, lastName, isActivated);
+    }
+
+  }
 
 }
