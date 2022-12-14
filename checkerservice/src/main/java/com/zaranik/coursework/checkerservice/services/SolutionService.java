e041a6c53cfb2abf40c90e641bb0bcd2f290aa14
GeniusDP <zaranikbz@gmail.com>
2022-12-14 14:02:12 +0200

    added checking reporting

--- checkerservice/src/main/java/com/zaranik/coursework/checkerservice/services/SolutionService.java
+++ checkerservice/src/main/java/com/zaranik/coursework/checkerservice/services/SolutionService.java
@@ -15,9 +15,14 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 @RequiredArgsConstructor
 public class SolutionService {
-
+  
   private final CustomSolutionRepository solutionRepository;
-
+  
+  private static final String COMPILED = "COMPILED";
+  private static final String NOT_COMPILED = "NOT_COMPILED";
+  private static final String TESTS_PASSED = "TESTS_PASSED";
+  private static final String SOME_TESTS_FAILED = "SOME_TESTS_FAILED";
+  
   @Value("${container.docker.start-command}")
   private String dockerStartCommand;
 
@@ -62,7 +67,30 @@ public class SolutionService {
         sb.append(line);
       }
     }
+    scanner.close();
+    processSolutionCheckingResult(solutionId, sb);
     return process.exitValue();
+  }
+
+  private Solution processSolutionCheckingResult(Long solutionId, StringBuilder sb) {
+    Solution solution = solutionRepository.findById(solutionId);
+    if(sb.indexOf(COMPILED) == -1) {
+      solution.setCompilationStatus(NOT_COMPILED);  
+      return solutionRepository.save(solution);
+    }
+    solution.setCompilationStatus(COMPILED);
+
+    if(sb.indexOf(TESTS_PASSED) == -1) {
+      solution.setTestingStatus(SOME_TESTS_FAILED);
+      return solutionRepository.save(solution);
+    }
+    solution.setTestingStatus(TESTS_PASSED);
+
+    /* 
+      here should be other logic about testing: prettier 
+    */
+
+    return solutionRepository.save(solution);
   }
 
 }
